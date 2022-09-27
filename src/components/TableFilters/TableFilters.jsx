205ad3dc5ed677a42336211f586369591df2a614
Artandrey <artandrey7777@gmail.com>
2022-09-28 01:55:19 +0300

    added heading styles

--- src/components/TableFilters/TableFilters.jsx
+++ src/components/TableFilters/TableFilters.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import FloatingColumnsFilter from '../FloatingColumnsFilter/FloatingColumnsFilter';
 import FloatingTableFilter from '../FloatingTableFilter/FloatingTableFilter';
 
@@ -8,16 +8,36 @@ const TableFilters = ({
     columnsFilterOptions,
     onColumnsFilterApply,
 }) => {
+    const [tableFilterOpened, setTableFilterOpened] = useState(false);
+    const [columnsFilterOpened, setColumnsFilterOpened] = useState(false);
+    const openFilter = useCallback(() => {
+        setTableFilterOpened(true);
+        setColumnsFilterOpened(false);
+    }, [setTableFilterOpened, setColumnsFilterOpened]);
+    const openColumns = useCallback(() => {
+        setTableFilterOpened(false);
+        setColumnsFilterOpened(true);
+    }, [setTableFilterOpened, setColumnsFilterOpened]);
     return (
         <div>
-            <FloatingTableFilter
-                onFilterApply={onTableFilterApply}
-                fields={filterFields}
-            />
-            <FloatingColumnsFilter
-                onFilterApply={onColumnsFilterApply}
-                columns={columnsFilterOptions}
-            />
+            <div>
+                <button onClick={openFilter}>Filter</button>
+                {tableFilterOpened && (
+                    <FloatingTableFilter
+                        onFilterApply={onTableFilterApply}
+                        fields={filterFields}
+                    />
+                )}
+            </div>
+            <div>
+                <button onClick={openColumns}>Columns</button>
+                {columnsFilterOpened && (
+                    <FloatingColumnsFilter
+                        onFilterApply={onColumnsFilterApply}
+                        columns={columnsFilterOptions}
+                    />
+                )}
+            </div>
         </div>
     );
 };
